18dc6ddb72097cf60561aef55517bbe725e254a1
ebatuhankaynak <ebatuhankaynak@gmail.com>
2023-12-01 22:02:28 +0300

    collect_commits now uses threading.

--- collect_commits.py
+++ collect_commits.py
@@ -1,12 +1,13 @@
 import requests
 import json
-from tqdm import tqdm
+import threading
+from pqdm.processes import pqdm
 
 GITHUB_API_TOKEN = "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"
 
 repo_names = {
     "Cli": "apache/commons-cli",
-    "Closure": "google/closure-compiler",
+    #"Closure": "google/closure-compiler",
     "Codec": "apache/commons-codec",
     "Compress": "apache/commons-compress",
     "Gson": "google/gson",
@@ -38,19 +39,20 @@ def get_commits(owner, repo):
             break
     return commits
 
-def get_commit_details(owner, repo, sha):
+def get_commit_details_task(args):
+    owner, repo, sha, index = args
     url = f"https://api.github.com/repos/{owner}/{repo}/commits/{sha}"
     headers = {'Authorization': f'token {GITHUB_API_TOKEN}'}
     try:
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
-            return response.json()
+            return index, response.json()
         else:
             print(f"Error fetching commit details for {sha}")
-        return None
+        return index, None
     except requests.exceptions.SSLError:
         print("SSL Error on commit")
-        return None
+        return index, None
 
 def save_to_json(data, filename):
     with open(filename, 'w') as file:
@@ -60,24 +62,29 @@ for project_name, repo_path in repo_names.items():
     commit_data = {}
     owner, repo = repo_path.split('/')
     commits = get_commits(owner, repo)
-    for commit in tqdm(list(reversed(commits))):
-        sha = commit['sha']
-        details = get_commit_details(owner, repo, sha)
 
-        commit_info = {
-            "author_name": details['commit']['author']['name'],
-            "files_changed": []
-        }
+    args_list = [(owner, repo, commit['sha'], index) for index, commit in enumerate(reversed(commits))]
 
-        for file in details['files']:
-            file_info = {
-                "filename": file['filename'],
-                "status": file['status'],
-                "additions": file['additions'],
-                "deletions": file['deletions']
+    commit_details_with_index  = pqdm(args_list, get_commit_details_task, n_jobs=10)
+    commit_details_with_index.sort(key=lambda x: x[0])
+    sorted_commit_details = [details for _, details in sorted(commit_details_with_index, key=lambda x: x[0])]
+
+    commit_data = {}
+    for details in sorted_commit_details:
+        if details:
+            sha = details['sha']
+            commit_info = {
+                "author_name": details['commit']['author']['name'],
+                "files_changed": [
+                    {
+                        "filename": file['filename'],
+                        "status": file['status'],
+                        "additions": file['additions'],
+                        "deletions": file['deletions']
+                    }
+                    for file in details['files']
+                ]
             }
-            commit_info["files_changed"].append(file_info)
-
-        commit_data[sha] = commit_info
+            commit_data[sha] = commit_info
 
     save_to_json(commit_data, f"raw/{project_name}.json")
